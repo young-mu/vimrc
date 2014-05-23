5c8be36e5479f147df80f35c12554cf94960ad1c
young <young.mwy@gmail.com>
2014-05-23 13:48:26 +0800

    archive plugins & add auto vim configuration script

---
+++ vimconf.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# ----- install VIM plugins -----
+
+mkdir /tmp/vim_plugins
+tar xzvf vim_plugins.tar.gz -C /tmp/vim_plugins
+
+# A
+cp /tmp/vim_plugins/a.vim ~/.vim/plugin/
+# miniBufExplorer
+cp /tmp/vim_plugins/minibufexpl.vim ~/.vim/plugin/
+# snipMate
+unzip /tmp/vim_plugins/snipMate.zip -d ~/.vim/
+# tagList
+unzip /tmp/vim_plugins/taglist_45.zip -d ~/.vim/
+# winManager
+unzip /tmp/winmanager.zip -d ~/.vim/
+
+rm -rf /tmp/vim_plugins
+
+# ----- replace vimrc -----
+cp vimrc ~/.vimrc
