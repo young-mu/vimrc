91af235a517af4c8d287619ad847a615d956116a
young <young.mwy@gmail.com>
2014-11-28 13:47:25 +0800

    1. support adding new snippet 2. remove trail space

--- update.sh
+++ update.sh
@@ -3,26 +3,31 @@
 # update from current snippets to vim_plugins.tar.gz
 
 if [[ ! -d ~/.vim/snippets ]]; then
-	echo "Error : no current snippets"	
-	exit 1
+    echo "Error : no current snippets"
+    exit 1
 fi
 
 tar xzf ./vim_plugins.tar.gz
 unzip -q ./vim_plugins/snipMate.zip -d ./vim_plugins/snipMate
 
-snippets=`ls -1 ./vim_plugins/snipMate/snippets`
+snippets=`ls -1 ~/.vim/snippets`
 
 for snippet in ${snippets}; do
-	if [[ `diff ~/.vim/snippets/${snippet} ./vim_plugins/snipMate/snippets/${snippet}` ]]; then
-		cp ~/.vim/snippets/${snippet} ./vim_plugins/snipMate/snippets/${snippet}
-		echo "update ${snippet}"
-	fi
+    diff ~/.vim/snippets/${snippet} ./vim_plugins/snipMate/snippets/${snippet} 2> /dev/null
+    ret=`echo $?`
+    if [[ ${ret} -eq 1 ]]; then
+        cp ~/.vim/snippets/${snippet} ./vim_plugins/snipMate/snippets/${snippet}
+        echo "update ${snippet}"
+    elif [[ ${ret} -eq 2 ]]; then
+        cp ~/.vim/snippets/${snippet} ./vim_plugins/snipMate/snippets/${snippet}
+        echo "add ${snippet}"
+    fi
 done
 
 cd ./vim_plugins/snipMate
 zip -rmq snipMate.zip *
 cd ../..
-mv ./vim_plugins/snipMate/snipMate.zip ./vim_plugins 
+mv ./vim_plugins/snipMate/snipMate.zip ./vim_plugins
 rm -rf ./vim_plugins/snipMate
 tar czf ./vim_plugins.tar.gz ./vim_plugins
 rm -rf ./vim_plugins
